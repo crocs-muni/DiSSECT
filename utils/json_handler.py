8c97bd3c93d8bdbcbd28c7b80eeeaee9fd5e0b45
Vladimir Sedlacek <vlada.sedlacek@gmail.com>
2020-06-23 13:15:15 +0200

    Split global utilities.

---
+++ utils/json_handler.py
@@ -0,0 +1,18 @@
+import json
+
+class IntegerEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Integer):
+            return int(obj)
+
+         # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+        
+def save_into_json(results, fname, mode = 'a'):
+    with open(fname, mode) as f:
+        json.dump(results, f, indent = 2 , cls = IntegerEncoder)
+        
+def load_from_json(fname):
+    with open(fname, 'r') as f:
+        results = json.load(f)
+    return results
