2d563c2e24a6c226177592e5804c1379a6ddf60d
Vladimir Sedlacek <vlada.sedlacek@gmail.com>
2020-07-03 11:50:22 +0200

    Update a22.

--- curve_analyzer/tests/a22/a22_division_pol_factorizations.py
+++ curve_analyzer/tests/a22/a22_division_pol_factorizations.py
@@ -3,22 +3,29 @@ from sage.all_cmdline import *   # import sage library
 from curve_analyzer.utils.curve_handler import *
 from curve_analyzer.tests.test_interface import *
 
-def a22_curve_function(curve, l_max):
-    E = curve.EC
-    curve_results = {'degs_list': [], 'lens': []}
-
-    for l in prime_range(l_max):
-        pol = curve.EC.division_polynomial(l)
-        fact = list(factor(pol))
-        degs = [x.degree() for x,_ in fact]
-        curve_results['degs_list'].append(degs)
-        curve_results['lens'].append(len(degs))
+def a22_curve_function(curve, l):
+    pol = curve.EC.division_polynomial(l)
+    fact = list(factor(pol))
+    # count multiplicities?
+    degs = [x.degree() for x, _ in fact]
+    curve_results = {'factorization': fact, 'degs_list': degs, 'len': len(degs)}
     return curve_results
 
-def compute_a22_results(l_max = 20, order_bound = 256, overwrite = False, curve_list = curves):
-    parameters = {'l_max': l_max}
-    compute_results('a22', a22_curve_function, parameters, order_bound, overwrite, curve_list = curve_list)
+def compute_a22_results(l_max = 20, order_bound = 256, overwrite = False, curve_list = curves, desc = ''):
+    global_params = {'l_range': prime_range(l_max)}
+    params_local_names = ['l']
+    #Add Ordered dict
+    compute_results('a22', a22_curve_function, global_params, params_local_names, order_bound, overwrite, curve_list = curve_list, desc = desc)
+
+def get_a22_captions(results):
+    captions = ['degs_lists', 'lens']
+    return captions
+
+def select_a22_results(curve_results):
+    degs_lists = [x['degs_list'] for x in curve_results]
+    lens = [x['len'] for x in curve_results]
+    selected_results = [degs_lists, lens]
+    return selected_results
 
 def pretty_print_a22_results(save_to_txt = True):
-    pretty_print_results('a22', [['lens']], ['Number of irreducible factors of l-th division polynomial'], save_to_txt = save_to_txt, res_sort_key = lambda x: 1 )
-
+    pretty_print_results('a22', get_a22_captions, select_a22_results, save_to_txt = save_to_txt)
