4198043c8d41b2228f8516566feca1b956262e59
vojta <451866@mail.muni.cz>
2020-07-23 17:08:10 +0200

    a25 new format

--- curve_analyzer/tests/a25/a25_trace_factorization.py
+++ curve_analyzer/tests/a25/a25_trace_factorization.py
@@ -8,10 +8,24 @@ def a25_curve_function(curve):
     return {'trace': len(list(factor(curve.trace)))}
 
 
-def compute_a25_results(curve_list, order_bound=256, overwrite=False):
-    parameters = {}
-    compute_results('a25', a25_curve_function, parameters, order_bound, overwrite)
+def compute_a25_results(curve_list, order_bound=256, overwrite=False, desc=''):
+    global_params = {}
+    params_local_names = []
+    compute_results(curve_list, 'a25', a25_curve_function, global_params, params_local_names, order_bound, overwrite,
+                    desc=desc)
+
+
+def get_a25_captions(results):
+    captions = ['factorization']
+    return captions
+
+
+def select_a25_results(curve_results):
+    selected_results = []
+    for key in curve_results.keys():
+        selected_results.append(curve_results[key])
+    return selected_results
 
 
 def pretty_print_a25_results(curve_list, save_to_txt=True):
-    pretty_print_results(curve_list, 'a25', [['trace']], ['trace factorization'], save_to_txt=save_to_txt)
+    pretty_print_results(curve_list, 'a25', get_a25_captions, select_a25_results, save_to_txt=save_to_txt)
