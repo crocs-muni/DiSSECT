86e0879c53120cae95a5ddd3ec98a36b06c29539
vojta <451866@mail.muni.cz>
2020-07-23 17:00:30 +0200

    a02 new format

--- curve_analyzer/tests/a02/a02_cm_disc_factorizations.py
+++ curve_analyzer/tests/a02/a02_cm_disc_factorizations.py
@@ -4,7 +4,7 @@ from curve_analyzer.tests.test_interface import pretty_print_results, compute_re
 
 # Computation of d_K (cm_disc), v (max_conductor) and factorization of D where D=t^2-4q = v^2*d_K
 # Returns a dictionary (keys: 'cm_disc', 'factorization', 'max_conductor') 
-def a2_curve_function(curve):
+def a02_curve_function(curve):
     E = curve.EC
     t = curve.trace
     q = curve.q
@@ -20,11 +20,24 @@ def a2_curve_function(curve):
     return curve_results
 
 
-def compute_a2_results(curve_list, order_bound=256, overwrite=False):
-    parameters = {}
-    compute_results('a2', a2_curve_function, parameters, order_bound, overwrite, curve_list=curve_list)
+def compute_a02_results(curve_list, order_bound=256, overwrite=False, desc=''):
+    global_params = {}
+    params_local_names = []
+    compute_results(curve_list, 'a02', a02_curve_function, global_params, params_local_names, order_bound, overwrite,
+                    desc=desc)
 
 
-def pretty_print_a2_results(curve_list, save_to_txt=True):
-    pretty_print_results(curve_list, 'a2', [['factorization'], ['max_conductor']],
-                         ['CM disc factorization', 'max conductor'], save_to_txt=save_to_txt)
+def get_a02_captions(results):
+    captions = results.keys()
+    return captions
+
+
+def select_a02_results(curve_results):
+    selected_results = []
+    for key in curve_results.keys():
+        selected_results.append(curve_results[key])
+    return selected_results
+
+
+def pretty_print_a02_results(curve_list, save_to_txt=True):
+    pretty_print_results(curve_list, 'a02', get_a02_captions, select_a02_results, save_to_txt=save_to_txt)
