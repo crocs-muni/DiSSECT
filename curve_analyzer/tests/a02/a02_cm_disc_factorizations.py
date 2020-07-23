7576ac1f2d5931d08ec6bea48b956485a3c53a10
vojta <451866@mail.muni.cz>
2020-07-23 10:50:29 +0200

    a02: removed * sage import, formatting

--- curve_analyzer/tests/a02/a02_cm_disc_factorizations.py
+++ curve_analyzer/tests/a02/a02_cm_disc_factorizations.py
@@ -1,5 +1,9 @@
-from sage.all_cmdline import *   # import sage library
+from sage.misc.functional import squarefree_part  # import sage library
+from sage.arith.misc import factor
+from sage.rings.all import Integers as ZZ
+from sage.functions.other import sqrt
 from curve_analyzer.tests.test_interface import pretty_print_results, compute_results
+
 
 # Computation of d_K (cm_disc), v (max_conductor) and factorization of D where D=t^2-4q = v^2*d_K
 # Returns a dictionary (keys: 'cm_disc', 'factorization', 'max_conductor') 
@@ -8,20 +12,22 @@ def a2_curve_function(curve):
     t = curve.trace
     q = curve.q
     curve_results = {'cm_disc': {}, 'factorization': {}}
-    D = t**2  - 4  * q
+    D = t ** 2 - 4 * q
     d = squarefree_part(D)
     disc = d
-    if Mod(d, 4 ) != 1 :
-        disc *= 4 
+    if d % 4 != 1:
+        disc *= 4
     curve_results['cm_disc'] = disc
     curve_results['factorization'] = list(factor(D))
-    curve_results['max_conductor'] = ZZ(sqrt(D/disc))
+    curve_results['max_conductor'] = ZZ(sqrt(D / disc))
     return curve_results
 
-def compute_a2_results(curve_list, order_bound = 256 , overwrite = False):
+
+def compute_a2_results(curve_list, order_bound=256, overwrite=False):
     parameters = {}
-    compute_results('a2', a2_curve_function, parameters, order_bound, overwrite, curve_list = curve_list)
+    compute_results('a2', a2_curve_function, parameters, order_bound, overwrite, curve_list=curve_list)
 
-def pretty_print_a2_results(curve_list, save_to_txt = True):
-    pretty_print_results(curve_list = curve_list, 'a2', [['factorization'], ['max_conductor']], ['CM disc factorization', 'max conductor'], save_to_txt = save_to_txt)
 
+def pretty_print_a2_results(curve_list, save_to_txt=True):
+    pretty_print_results(curve_list, 'a2', [['factorization'], ['max_conductor']],
+                         ['CM disc factorization', 'max conductor'], save_to_txt=save_to_txt)
