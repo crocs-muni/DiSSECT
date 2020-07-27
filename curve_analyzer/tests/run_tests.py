3840160a0d4849e1837c4f8698e9d0866202d60a
Vladimir Sedlacek <vlada.sedlacek@gmail.com>
2020-07-27 17:42:49 +0200

    Add CLI support for running tests.

---
+++ curve_analyzer/tests/run_tests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env sage
+
+import argparse
+import sys
+from curve_analyzer.utils.curve_handler import import_curves
+
+parser = argparse.ArgumentParser(description='Run tests on a selected subset of standard or simulated curves.')
+parser.add_argument('test_name', metavar='test_name', type=str, nargs=1, action='store', help='the test identifier, e.g., a02')
+parser.add_argument('curve_type', metavar='curve_type', type=str, nargs=1, help='the type of curves to be tested; must be one of the following: std (all standard curves), sim (all simulated curves), sample (curves secp112r1, secp192r1, secp256r1), all (all curves in the database)')
+parser.add_argument('-v', '--verbosity', action='store_true')
+
+args = parser.parse_args()
+tn = args.test_name[0]
+module_name = tn + '.' + tn
+try:
+    __import__(module_name)
+except ModuleNotFoundError:
+    print("please enter a valid test identifier, e.g., a02")
+    exit()
+test_function = getattr(sys.modules[module_name], "compute_" + tn + "_results")
+
+ct = args.curve_type[0]
+if not ct in ["std", "sim", "sample", "all"]:
+    print("curve_type must be one of std, sim, sample, all")
+    exit()
+curves_list = import_curves(ct, args.verbosity)
+
+if args.verbosity:
+    print("\nInitializing test " + tn + ", hold on to your hat!")
+test_function(curves_list)
