369b646a7651d538340d9884125590d52cc2e26e
Vladimir Sedlacek <vlada.sedlacek@gmail.com>
2020-07-27 17:41:31 +0200

    Improve the flexibility of curve database loading.

--- curve_analyzer/utils/curve_handler.py
+++ curve_analyzer/utils/curve_handler.py
@@ -23,23 +23,33 @@ def import_curve_db(root = CURVE_PATH, ignore_sim = True):
                 curve_db[source + suffix] = json.load(f)
     return curve_db
 
-def curve_gen(curve_db):
+def curve_gen(curve_db, type, verbose = False):
     sources = curve_db.keys()
     for source in sources:
         curves = curve_db[source]['curves']
         for curve in curves:
+            name = curve['name']
+            if type == "std" and "sim" in name:
+                continue
+            if type == "sim" and not "sim" in name:
+                continue
+            if type == "sample" and not name in ["secp112r1", "secp192r1", "secp256r1"]:
+                continue
+            if verbose:
+                print(curve['name'])
             yield CustomCurve(curve)
 
-def custom_curves(curve_db):
-    return [c for c in curve_gen(curve_db)]
+def custom_curves(curve_db, type, verbose = False):
+    return [c for c in curve_gen(curve_db, type, verbose)]
 
 def custom_curves_sorted_by_name(curve_db):
     return sorted(custom_curves(curve_db), key = lambda item: item.name)
 
-# def init_curve_db():
-print("importing database")
-curve_db = import_curve_db(CURVE_PATH)
-curves = sorted(custom_curves(curve_db))
-curves_sample = [c for c in curves if c.name in ["secp112r1", "secp192r1", "secp256r1"]]
-curves_sim = [c for c in curves if "sim" in c.name]
-curves_std = [c for c in curves if not "sim" in c.name]
+def import_curves(type = "sample", verbose = False):
+    print("Importing database...")
+    ignore_sim = True
+    if type in ["sim", "all"]:
+        ignore_sim = False
+    curve_db = import_curve_db(CURVE_PATH, ignore_sim)
+    curve_list = sorted(custom_curves(curve_db, type, verbose))
+    return curve_list
