f91ada152dd34eff4f093f01a4b7703704eff2c7
vojta <451866@mail.muni.cz>
2020-07-27 10:49:10 +0200

    a04 new format

--- curve_analyzer/tests/a04/a04_near_order_factorizations.py
+++ curve_analyzer/tests/a04/a04_near_order_factorizations.py
@@ -2,6 +2,7 @@ from sage.all import ecm
 from curve_analyzer.tests.test_interface import pretty_print_results, compute_results, remove_values_from_list, timeout, \
     ints_before_strings
 
+TIME = 10
 
 def attempt_factor(n, t):
     try:
@@ -11,52 +12,64 @@ def attempt_factor(n, t):
     return factorization
 
 
-# Factorizations (lists of prime factors) of k*n+-1 where k=1,..,k_max
-# Returns list of lists
-def near_order_factorizations(n, sign='+', k_max=10, t=10):
-    factorizations = []
+def near_order_factorizations(n, sign='+', k=10, t=10):
+
     assert sign in ['+', '-']
-    for k in range(1, k_max + 1):
-        if sign == '+':
-            m = k * n + 1
-        elif sign == '-':
-            m = k * n - 1
-        try:
-            factorizations.append(attempt_factor(m, t))
-        except:
-            continue
+
+    if sign == '+':
+        m = k * n + 1
+    elif sign == '-':
+        m = k * n - 1
+    try:
+        return attempt_factor(m, t)
+    except:
+        return None
     return factorizations
 
 
 def largest_factor_bitlen(factorization):
     try:
-        bitlen = factorization[-1].nbits()
+        bitlen = factorization.nbits()
     except:
         bitlen = '-'
     return bitlen
 
 
-def a4_curve_function(curve, k_max, t):
+def a04_curve_function(curve, k):
+    t = TIME
     curve_results = {'plus': {}, 'minus': {}}
-    curve_results['plus']['factorizations'] = near_order_factorizations(curve.order, '+', k_max, t)
+    curve_results['plus']['factorization'] = near_order_factorizations(curve.order, '+', k, t)
     curve_results['plus']['largest_factor_bitlens'] = list(
-        map(largest_factor_bitlen, curve_results['plus']['factorizations']))
-    curve_results['plus']['min_largest_factor_bitlen'] = min(
-        remove_values_from_list(curve_results['plus']['largest_factor_bitlens'], '-'))
-    curve_results['minus']['factorizations'] = near_order_factorizations(curve.order, '-', k_max, t)
+        map(largest_factor_bitlen, curve_results['plus']['factorization']))
+    #curve_results['plus']['min_largest_factor_bitlen'] = min(
+    #    remove_values_from_list(curve_results['plus']['largest_factor_bitlens'], '-'))
+    curve_results['minus']['factorizations'] = near_order_factorizations(curve.order, '-', k, t)
     curve_results['minus']['largest_factor_bitlens'] = list(
         map(largest_factor_bitlen, curve_results['minus']['factorizations']))
-    curve_results['minus']['min_largest_factor_bitlen'] = min(
-        remove_values_from_list(curve_results['minus']['largest_factor_bitlens'], '-'))
+    #curve_results['minus']['min_largest_factor_bitlen'] = min(
+    #    remove_values_from_list(curve_results['minus']['largest_factor_bitlens'], '-'))
     return curve_results
 
 
-def compute_a4_results(curve_list, k_max=10, t=10, order_bound=256, overwrite=False):
-    parameters = {'k_max': k_max, 't': t}
-    compute_results(curve_list, 'a4', a4_curve_function, parameters, order_bound, overwrite)
+def compute_a04_results(curve_list, k_max=10, order_bound=256, overwrite=False, desc=''):
+    global_params = {"k_max": range(1,k_max + 1)}
+    params_local_names = ["k"]
+    compute_results(curve_list, 'a04', a04_curve_function, global_params, params_local_names, order_bound, overwrite,
+                    desc=desc)
 
 
-def pretty_print_a4_results(curve_list, save_to_txt=True):
-    pretty_print_results(curve_list, 'a4', [['plus', 'largest_factor_bitlens'], ['minus', 'largest_factor_bitlens']],
-                         ['largest_factor_bitlens (+)', 'largest_factor_bitlens (-)'], res_sort_key=ints_before_strings,
-                         save_to_txt=save_to_txt)
+def get_a04_captions(results):
+    captions = [['plus', 'largest_factor_bitlens'], ['minus', 'largest_factor_bitlens']], ['largest_factor_bitlens (+)',
+                                                                                           'largest_factor_bitlens (-)']
+    return captions
+
+
+def select_a04_results(curve_results):
+    selected_results = []
+    for key in curve_results.keys():
+        selected_results.append(curve_results[key])
+    return selected_results
+
+
+def pretty_print_a04_results(curve_list, save_to_txt=True):
+    pretty_print_results(curve_list, 'a04', get_a04_captions, select_a04_results, save_to_txt=save_to_txt)
