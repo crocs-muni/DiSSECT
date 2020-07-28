edb262b7f43fb6027be8157309f5766dd4d473c4
vojta <451866@mail.muni.cz>
2020-07-28 13:07:35 +0200

    brief documentation for curve_handler and custom_curve

--- curve_analyzer/utils/custom_curve.py
+++ curve_analyzer/utils/custom_curve.py
@@ -1,50 +1,59 @@
 from sage.all import EllipticCurve, ZZ, GF  # import sage library
 
-def montgomery_to_short_weierstrass(F, A, B, x, y):
-    a = F( (3 -A**2 )/(3 *B**2 ) )
-    b = F( (2 *A**3  - 9 *A)/(27 *B**3 ) )
-    u = F( (3 *x+A)/(3 *B) )
-    v = F( y/B )
-    assert (u, v) in EllipticCurve(F, [a,b])
-    return (a, b, u, v)
 
-def twisted_edwards_to_montgomery(F, a, d, u, v, scaling = True):
-    A = F( (2 *a + 2 *d)/(a- d) )
-    B = F( 4 /(a - d) )
+# Converting functions using formulas in https://tools.ietf.org/id/draft-struik-lwip-curve-representations-00.html
+
+
+def montgomery_to_short_weierstrass(F, A, B, x, y):
+    a = F((3 - A ** 2) / (3 * B ** 2))
+    b = F((2 * A ** 3 - 9 * A) / (27 * B ** 3))
+    u = F((3 * x + A) / (3 * B))
+    v = F(y / B)
+    assert (u, v) in EllipticCurve(F, [a, b])
+    return a, b, u, v
+
+
+def twisted_edwards_to_montgomery(F, a, d, u, v, scaling=True):
+    A = F((2 * a + 2 * d) / (a - d))
+    B = F(4 / (a - d))
     if not B.is_square():
         scaling = False
-    s = F(1 /B).sqrt()
-    x = F( (1  + v)/(1  - v) )
-    y = F( (1  + v)/((1  - v) * u) )
+    s = F(1 / B).sqrt()
+    x = F((1 + v) / (1 - v))
+    y = F((1 + v) / ((1 - v) * u))
     if scaling:
-        assert (x, y/s) in EllipticCurve(F, [0 , A, 0 , 1 , 0 ])
-        return (A, 1 , x, y/s)
-    return (A, B, x, y)
+        assert (x, y / s) in EllipticCurve(F, [0, A, 0, 1, 0])
+        return A, 1, x, y / s
+    return A, B, x, y
+
 
 def twisted_edwards_to_short_weierstrass(F, aa, d, x, y):
     A, B, x, y = twisted_edwards_to_montgomery(F, aa, d, x, y, True)
     a, b, u, v = montgomery_to_short_weierstrass(F, A, B, x, y)
-    assert (u, v) in EllipticCurve(F, [a,b])
-    return (a, b, u, v)
+    assert (u, v) in EllipticCurve(F, [a, b])
+    return a, b, u, v
 
+
+# Class for unified representation of curves from databases
 class CustomCurve:
-    
+
     def __init__(self, db_curve):
-        '''the "fixed" part of attributes'''
+        """the "fixed" part of attributes"""
         self.name = db_curve['name']
         self.order = ZZ(db_curve['order'])
         self.source = db_curve['category']
-        self.field = db_curve['field']
+        self.field_desc = db_curve['field']
         self.form = db_curve['form']
         self.params = db_curve['params']
         self.desc = db_curve['desc']
         self.cofactor = ZZ(db_curve['cofactor'])
-        self.nbits =  self.order.nbits()
+        self.nbits = self.order.nbits()
+        self.field = None
         self.EC = None
         self.generator = None
         self.q = None
         self.trace = None
-        '''the "variable" part of attributes'''    
+        '''the "variable" part of attributes'''
         try:
             self.seed = db_curve['seed']
         except KeyError:
@@ -55,15 +64,14 @@ class CustomCurve:
         except TypeError:
             self.x = None
             self.y = None
-    #         self.characteristics = db_curve['characteristics']
         self.set()
 
-    def set_generator(self, coord1, coord2, binary = False):
-        if self.x == None or self.y == None:
+    def set_generator(self, coord1, coord2, binary=False):
+        if self.x is None or self.y is None:
             self.generator = None
         else:
             if binary:
-                self.generator = self.EC(K.fetch_int(coord1), K.fetch_int(coord2))
+                self.generator = self.EC(self.field.fetch_int(coord1), self.field.fetch_int(coord2))
             else:
                 self.generator = self.EC(coord1, coord2)
 
@@ -73,22 +81,25 @@ class CustomCurve:
         if self.form == "Weierstrass":
             a = ZZ(self.params['a'])
             b = ZZ(self.params['b'])
-            if self.field['type'] == "Prime":
-                p = ZZ(self.field['p'])
-                self.EC = EllipticCurve(GF(p), [a,b])
-                self.set_generator(x,y)
-            elif self.field['type'] == "Binary":
-                exponents = list(self.field['poly'].values())
-                exponents.append(0 )
-                F = GF(2 )['w']; (w,) = F._first_ngens(1)
-                modulus = 0 
+            if self.field_desc['type'] == "Prime":
+                p = ZZ(self.field_desc['p'])
+                F = GF(p)
+                self.EC = EllipticCurve(F, [a, b])
+                self.field = F
+                self.set_generator(x, y)
+            elif self.field_desc['type'] == "Binary":
+                exponents = list(self.field_desc['poly'].values())
+                exponents.append(0)
+                F = GF(2)['w']
+                (w,) = F._first_ngens(1)
+                modulus = 0
                 for e in exponents:
-                    modulus += w**e
-                m = ZZ(self.field['poly']['m'])
-                K = GF(2 **m, 'w', modulus)
-                self.EC = EllipticCurve(K, [1 ,K.fetch_int(ZZ(a)),0 ,0 ,K.fetch_int(ZZ(b))]) #xy, x^2, y, x, 1
-                # print(self.EC)
+                    modulus += w ** e
+                m = ZZ(self.field_desc['poly']['m'])
+                K = GF(2 ** m, 'w', modulus)
+                self.EC = EllipticCurve(K, [1, K.fetch_int(ZZ(a)), 0, 0, K.fetch_int(ZZ(b))])  # xy, x^2, y, x, 1
                 self.generator = None
+                self.field = K
                 # self.set_generator(x,y)
                 # needs fixing, originally:
                 # self.generator = self.EC(K.fetch_int(x), K.fetch_int(y))
@@ -96,40 +107,45 @@ class CustomCurve:
         elif self.form == "Montgomery":
             A = ZZ(self.params['a'])
             B = ZZ(self.params['b'])
-            p = ZZ(self.field['p'])
+            p = ZZ(self.field_desc['p'])
             F = GF(p)
+            self.field = F
             a, b, u, v = montgomery_to_short_weierstrass(F, A, B, x, y)
-            self.EC = EllipticCurve(F, [a,b])
-            self.set_generator(u,v)
+            self.EC = EllipticCurve(F, [a, b])
+            self.set_generator(u, v)
 
         elif self.form in ["Edwards", "TwistedEdwards"]:
-            #we assume c=1
+            # we assume c=1
             if self.form == "Edwards":
-                aa = 1 
+                aa = 1
             if self.form == "TwistedEdwards":
                 aa = ZZ(self.params['a'])
             d = ZZ(self.params['d'])
-            p = ZZ(self.field['p'])
+            p = ZZ(self.field_desc['p'])
             F = GF(p)
             a, b, xx, yy = twisted_edwards_to_short_weierstrass(F, aa, d, x, y)
-            self.EC = EllipticCurve(F, [a,b])
-            self.set_generator(xx,yy)
+            self.EC = EllipticCurve(F, [a, b])
+            self.set_generator(xx, yy)
         else:
             self.EC = "Not implemented"
 
         self.q = self.EC.base_field().order()
-        self.trace = self.q + 1  - self.order * self.cofactor
+        self.trace = self.q + 1 - self.order * self.cofactor
 
     def __repr__(self):
-        return self.name + ": " + str(self.nbits) + "-bit curve in " + self.form + " form over " + self.field['type'] + " field" 
-    
+        return self.name + ": " + str(self.nbits) + "-bit curve in " + self.form + " form over " + self.field_desc[
+            'type'] + " field"
+
     def __str__(self):
-        return self.name + ": " + str(self.nbits) + "-bit curve in " + self.form + " form over " + self.field['type'] + " field" 
+        return self.name + ": " + str(self.nbits) + "-bit curve in " + self.form + " form over " + self.field_desc[
+            'type'] + " field"
 
     def __lt__(self, other):
         return (self.order, self.name) < (other.order, other.name)
 
-class SimulatedWeierstrassCurve():
+
+# Not using for now
+class SimulatedWeierstrassCurve:
     def __init__(self, F, a, b, seed):
         self.F = F
         self.nbits = self.F.order().nbits()
@@ -141,9 +157,9 @@ class SimulatedWeierstrassCurve():
 
     def __repr__(self):
         return str(self.nbits) + "-bit Weierstrass curve"
-    
+
     def __str__(self):
         return str(self.nbits) + "-bit Weierstrass curve"
 
     def __lt__(self, other):
-        return (self.order) < (other.order)
+        return self.order < other.order
