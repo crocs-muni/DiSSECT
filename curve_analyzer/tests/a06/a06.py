686f9480e3227dce56283815f7552a7d1b202b6e
vojta <451866@mail.muni.cz>
2020-08-10 10:27:31 +0200

    a06 pretty print finished

--- curve_analyzer/tests/a06/a06.py
+++ curve_analyzer/tests/a06/a06.py
@@ -1,6 +1,7 @@
 from sage.all import kronecker
 from curve_analyzer.tests.test_interface import pretty_print_results, compute_results
 
+
 def ext_card(E, q, card_low, deg):
     '''returns curve cardinality over deg-th relative extension'''
     tr = q + 1 - card_low
@@ -10,11 +11,13 @@ def ext_card(E, q, card_low, deg):
     card_high = q ** deg + 1 - s_new
     return card_high
 
+
 def ext_cm_disc(E, q, card_low, deg):
     '''returns the CM discriminant (up to a square) over deg-th relative extension'''
     card_high = ext_card(E, q, card_low, deg)
     ext_tr = q ** deg + 1 - card_high
     return ext_tr ** 2 - 4 * q ** deg
+
 
 def a06_curve_function(curve, l, deg):
     '''returns the Kronecker symbol of the CM discriminant over the deg-th relative extension with respect to l'''
@@ -23,8 +26,8 @@ def a06_curve_function(curve, l, deg):
     curve_results = {}
     order = curve.order * curve.cofactor
     cm_disc = ext_cm_disc(E, q, order, deg)
-    while cm_disc % l**2 == 0:
-        cm_disc = cm_disc / l**2
+    while cm_disc % l ** 2 == 0:
+        cm_disc = cm_disc / l ** 2
 
     if l == 2 and cm_disc % 4 != 1:
         curve_results["kronecker"] = 0
@@ -32,16 +35,25 @@ def a06_curve_function(curve, l, deg):
         curve_results["kronecker"] = kronecker(cm_disc, l)
     return curve_results
 
+
 def compute_a06_results(curve_list, desc=''):
     compute_results(curve_list, 'a06', a06_curve_function, desc=desc)
 
+
 def get_a06_captions(results):
-    # TO DO
-    pass
+    return ["kronecker"]
+
 
 def select_a06_results(curve_results):
-    # TO DO
-    pass
+    keys = ["kronecker"]
+    selected_results = []
+    for key in keys:
+        selected_key = []
+        for x in curve_results:
+            selected_key.append(x[key])
+        selected_results.append(selected_key)
+    return selected_results
+
 
 def pretty_print_a06_results(curve_list, save_to_txt=True):
     pretty_print_results(curve_list, 'a06', get_a06_captions, select_a06_results, save_to_txt=save_to_txt)
