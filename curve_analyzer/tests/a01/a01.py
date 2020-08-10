141ab90dc7fadc2eec64119bdcd36549a80793ba
vojta <451866@mail.muni.cz>
2020-08-10 10:22:00 +0200

    a01 pretty print finished

--- curve_analyzer/tests/a01/a01.py
+++ curve_analyzer/tests/a01/a01.py
@@ -1,5 +1,6 @@
-from sage.all import GF, elliptic_curves
+from sage.all import GF
 from curve_analyzer.tests.test_interface import pretty_print_results, compute_results
+
 
 def a01_curve_function(curve, deg):
     '''returns the orders of the two generators of the curve over the deg-th relative extension'''
@@ -19,13 +20,19 @@ def a01_curve_function(curve, deg):
 def compute_a01_results(curve_list, desc=''):
     compute_results(curve_list, 'a01', a01_curve_function, desc=desc)
 
+
 def get_a01_captions(results):
-    # TO DO
-    pass
+    return ['ord1', 'ord2']
+
 
 def select_a01_results(curve_results):
-    # TO DO
-    pass
+    keys = ['ord1', 'ord2']
+    selected_results = []
+    for key in keys:
+        for x in curve_results:
+            selected_results.append(x[key])
+    return selected_results
+
 
 def pretty_print_a01_results(curve_list, save_to_txt=True):
     pretty_print_results(curve_list, 'a01', get_a01_captions, select_a01_results, save_to_txt=save_to_txt)
