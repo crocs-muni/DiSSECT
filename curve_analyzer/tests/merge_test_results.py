615f4931830f4abf361ec64cd087c188f11ced59
Vladimir Sedlacek <vlada.sedlacek@gmail.com>
2020-09-09 17:47:39 +0200

    Refactor the merging process to be less fragile.

--- curve_analyzer/tests/merge_test_results.py
+++ curve_analyzer/tests/merge_test_results.py
@@ -7,7 +7,6 @@ import re
 from collections import OrderedDict
 
 from curve_analyzer.definitions import TEST_PATH
-from curve_analyzer.tests.test_interface import get_timestamp
 from curve_analyzer.utils.json_handler import load_from_json, save_into_json
 
 parser = argparse.ArgumentParser(description='Test results merger')
@@ -16,36 +15,30 @@ args = parser.parse_args()
 test_name = args.test_name
 assert re.search(r'[ais][0-9][0-9]', test_name)
 
-# iterate through partial results and merge them together
-partial_results_filenames = []
-merged = OrderedDict({})
-for root, _, files in os.walk(os.path.join(TEST_PATH, test_name)):
-    for file in files:
-        if file.split('.')[-1] != 'json' or "part" not in file:
-            continue
-        fname = os.path.join(root, file)
-        partial_results_filenames.append(fname)
-        with open(fname, 'r') as f:
-            results = json.load(f)
-            merged.update(results)
-
-# merge all the results with the original ones, sort and save them as a temp file
+# initialize original results
 results_file_name = os.path.join(TEST_PATH, test_name, test_name + '.json')
 if os.path.isfile(results_file_name):
     total_results = load_from_json(results_file_name)
 else:
     total_results = {}
 
-total_results.update(sorted(merged.items()))
-total_results_ordered = OrderedDict(sorted(total_results.items()))
-merged_file_name = os.path.join(TEST_PATH, test_name, test_name + '_' + get_timestamp() + '.tmp')
-save_into_json(total_results_ordered, merged_file_name, 'w+')
+# iterate through partial results and merge them together with the original ones, then delete them
+for root, _, files in os.walk(os.path.join(TEST_PATH, test_name)):
+    for file in files:
+        if file.split('.')[-1] != 'json' or "part" not in file:
+            continue
+        fname = os.path.join(root, file)
+        with open(fname, 'r') as f:
+            partial_results = OrderedDict(json.load(f))
+        partial_results_sorted = sorted(partial_results.items())
+        total_results.update(partial_results_sorted)
+        tmp_file_name = os.path.join(TEST_PATH, test_name, test_name + '.tmp')
+        save_into_json(total_results, tmp_file_name, 'w+')
+        os.remove(fname)
 
-# delete the original results and the partial results and rename the temp file
-for file in partial_results_filenames:
-    os.remove(file)
-try:
-    os.remove(results_file_name)
-except OSError:
-    pass
-os.rename(merged_file_name, results_file_name)
+        # delete the old result file and rename the temp one
+        try:
+            os.remove(results_file_name)
+        except OSError:
+            pass
+        os.rename(tmp_file_name, results_file_name)
