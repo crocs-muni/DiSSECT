9bbac7456e18fa644fda165d95086c1949e11de3
Vladimir Sedlacek <vlada.sedlacek@gmail.com>
2020-09-09 18:50:51 +0200

    Fix some bugs introduced by parallelization; new test runs now correctly detect existing results.

--- curve_analyzer/tests/test_interface.py
+++ curve_analyzer/tests/test_interface.py
@@ -64,29 +64,34 @@ class Logs:
 
 # Deduces paths to JSON files from the test name
 def init_json_paths(test_name, desc=''):
-    path_json = os.path.join(TEST_PATH, test_name, test_name + desc + '_' + get_timestamp() + '.json')
+    path_main_json = os.path.join(TEST_PATH, test_name, test_name + '.json')
+    path_json = os.path.join(TEST_PATH, test_name, test_name + '_' + desc + '_' + get_timestamp() + '.json')
     path_tmp = os.path.join(TEST_PATH, test_name, 'tmp.json')
     path_params = os.path.join(TEST_PATH, test_name, test_name + '.params')
     if not os.path.exists(path_json):
         save_into_json({}, path_json, 'w')
-    return path_json, path_tmp, path_params
+    return path_main_json, path_json, path_tmp, path_params
 
 
 # Tries to run tests for each individual curve; called by compute_results
-def update_curve_results(curve, curve_function, params_global, params_local_names, results, log_obj):
+def update_curve_results(curve, curve_function, params_global, params_local_names, old_results, log_obj):
     log_obj.write_to_logs("Processing curve " + curve.name + ":", newlines=1)
-    if curve.name not in results:
-        results[curve.name] = {}
+    new_results = {}
+    if curve.name not in old_results:
+        new_results[curve.name] = {}
+    else:
+        new_results[curve.name] = old_results[curve.name]
 
     for params_local_values in itertools.product(*params_global.values()):
         params_local = dict(zip(params_local_names, params_local_values))
         log_obj.write_to_logs("\tProcessing params " + str(params_local), frmt='{:.<60}')
-        if str(params_local) in results[curve.name]:
+        # print(params_local, results[curve.name])
+        if curve.name in old_results and str(params_local) in old_results[curve.name]:
             log_obj.write_to_logs("Already computed", newlines=1)
         else:
-            results[curve.name][str(params_local)] = curve_function(curve, *params_local_values)
+            new_results[curve.name][str(params_local)] = curve_function(curve, *params_local_values)
             log_obj.write_to_logs("Done", newlines=1)
-    return results[curve.name]
+    return new_results[curve.name]
 
 
 # A universal function for running tests on curve lists; it is called by each test file which has its own curve function.
@@ -95,9 +100,10 @@ def compute_results(curve_list, test_name, curve_function, desc=''):
     if curve_list == []:
         print("No input curves found, terminating the test.")
         return
-    json_file, tmp_file, params_file = init_json_paths(test_name, desc)
+    main_json_file, json_file, tmp_file, params_file = init_json_paths(test_name, desc)
     log_obj = Logs(test_name, desc)
-    results = load_from_json(json_file)
+    old_results = load_from_json(main_json_file)
+    new_results = {}
     if not os.path.exists(params_file):
         print("No parameter file found, terminating the test.")
         return
@@ -125,15 +131,16 @@ def compute_results(curve_list, test_name, curve_function, desc=''):
     for curve in curve_list:
         start_time = time.time()
 
-        results[curve.name] = update_curve_results(curve, curve_function, params_global, params_local_names, results,
-                                                   log_obj)
+        new_results[curve.name] = update_curve_results(curve, curve_function, params_global, params_local_names,
+                                                       old_results,
+                                                       log_obj)
 
         end_time = time.time()
         diff_time = end_time - start_time
         total_time += diff_time
 
         log_obj.write_to_logs("Done, time elapsed: " + str(diff_time), newlines=2)
-        save_into_json(results, tmp_file, 'w')
+        save_into_json(new_results, tmp_file, 'w')
 
     log_obj.write_to_logs(80 * '.' + "\n" + "Finished, total time elapsed: " + str(total_time) + "\n\n" + 80 * '#',
                           newlines=3)
@@ -150,8 +157,8 @@ def init_txt_paths(test_name, desc=''):
 
 # Visualizes test results from the relevant JSON; the functions get_captions are select_results are provided by each test separately
 def pretty_print_results(curve_list, test_name, get_captions, select_results, curve_sort_key="bits", save_to_txt=True):
-    path_json, _, _ = init_json_paths(test_name)
-    results = load_from_json(path_json)
+    path_main_json, _, _, _ = init_json_paths(test_name)
+    results = load_from_json(path_main_json)
 
     captions = get_captions(results)
     headlines = ['name', 'bits']
