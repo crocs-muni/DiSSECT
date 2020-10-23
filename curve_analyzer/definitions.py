874c6e16cfee7a28e1e3df1862b9dc143a3636d2
vojta <451866@mail.muni.cz>
2020-10-23 12:50:43 +0200

    tests structures generation

--- curve_analyzer/definitions.py
+++ curve_analyzer/definitions.py
@@ -6,3 +6,5 @@ CURVE_PATH_SIM = ROOT_DIR + '/curves_json_sim'
 TEST_PATH = ROOT_DIR + '/tests'
 PARALLEL_RESULTS_PATH = ROOT_DIR + '/utils/parallel/results'
 X962_PATH = ROOT_DIR + '/utils/parallel/x962'
+TEST_MODULE_PATH = 'curve_analyzer.tests'
+TEST_prefixes = ['a','i']
