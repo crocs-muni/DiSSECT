2a3aa050c13fe9236af350bcd8345566323b506c
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2020-10-25 08:43:59 +0100

    Change wording.

--- params.py
+++ params.py
@@ -10,10 +10,10 @@ written = ""
 for test in params:
 	
 	name,to_write = test.split(":",1)
-	Path("curve_analyzer/tests/"+name).mkdir(parents=True, exist_ok=True)	
+	Path("curve_analyzer/tests/" + name).mkdir(parents=True, exist_ok=True)	
 	to_write = to_write.strip()
-	full_name = "./curve_analyzer/tests/"+name+"/"+name+".params"
+	full_name = "./curve_analyzer/tests/"+ name + "/" + name + ".params"
 	with open(full_name, 'w') as f:
 		f.write(to_write)
-	written +=name + ", "
-print("Params files created for "+written[:-2])
+	written += name + ", "
+print("Params files created/updated for "+ written[:-2])
