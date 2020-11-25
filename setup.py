99bf223e2374189e74eb825d57cc9cbb35a485d1
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2020-11-25 06:01:27 +0100

    Update info.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 import os
 
-setup(name='curve_analyzer',
-      # version='0.1',
-      # description='some description',
-      # url='#',
+setup(name='DiSSECT',
+      version='0.2',
+      description='Distinguisher of Standard & Simulated Elliptic Curves through Testing.',
+      url='https://gitlab.fi.muni.cz/x408178/curve_analyzer',
       author='SeSuSy',
       # author_email='email',
-      # license='MIT',
+      license='MIT',
       packages=find_packages())
 
 os.system("curve_analyzer/utils/parallel/setup.py")
