395967c969204e3781d610f754b8ada127263b26
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2020-11-29 18:46:31 +0100

    Refactor via pathlib.

--- curve_analyzer/definitions.py
+++ curve_analyzer/definitions.py
@@ -1,10 +1,9 @@
-import os
+from pathlib import Path
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
-CURVE_PATH = ROOT_DIR + '/curves_json'
-CURVE_PATH_SIM = ROOT_DIR + '/curves_json_sim'
-TEST_PATH = ROOT_DIR + '/tests'
-PARALLEL_RESULTS_PATH = ROOT_DIR + '/utils/parallel/results'
-X962_PATH = ROOT_DIR + '/utils/parallel/x962'
+ROOT_DIR = Path(__file__).parent   # This is your Project Root
+CURVE_PATH = Path(ROOT_DIR, 'curves_json')
+CURVE_PATH_SIM = Path(ROOT_DIR, 'curves_json_sim')
+TEST_PATH = Path(ROOT_DIR, 'tests')
+PARALLEL_RESULTS_PATH = Path(ROOT_DIR, 'utils/parallel/results')
+X962_PATH = Path(ROOT_DIR, 'utils/parallel/x962')
 TEST_MODULE_PATH = 'curve_analyzer.tests'
-TEST_prefixes = ['a','i']
