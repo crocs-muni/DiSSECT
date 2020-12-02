252a71613b5bea0dcdb07ba0ab92fd6959ae82d0
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2020-12-02 15:57:44 +0100

    Refactor via pathlib.

--- curve_analyzer/tests/merge_test_results.py
+++ curve_analyzer/tests/merge_test_results.py
@@ -5,6 +5,7 @@ import json
 import os
 import re
 
+from pathlib import Path
 from curve_analyzer.definitions import TEST_PATH
 from curve_analyzer.utils.json_handler import load_from_json, save_into_json
 
@@ -30,28 +31,24 @@ test_name = args.test_name
 assert re.search(r'[ais][0-9][0-9]', test_name)
 
 # initialize original results
-results_file_name = os.path.join(TEST_PATH, test_name, test_name + '.json')
-if os.path.isfile(results_file_name):
+results_file_name = Path(TEST_PATH, test_name, test_name + '.json')
+if results_file_name.is_file():
     total_results = load_from_json(results_file_name)
 else:
     total_results = {}
 
-# iterate through partial results and merge them together with the original ones, then delete them
-for root, _, files in os.walk(os.path.join(TEST_PATH, test_name)):
-    for file in files:
-        if file.split('.')[-1] != 'json' or "part" not in file:
-            continue
-        fname = os.path.join(root, file)
-        with open(fname, 'r') as f:
-            partial_results = json.load(f)
-        dict_update_rec(total_results, partial_results)
-        tmp_file_name = os.path.join(TEST_PATH, test_name, test_name + '.tmp')
-        save_into_json(total_results, tmp_file_name, 'w+')
-        os.remove(fname)
+# iterate through partial results in the same folder and merge them together with the original ones, then delete them
+files = [item for item in Path(TEST_PATH, test_name).iterdir() if item.is_file()]
+for file in files:
+    if file.suffix != '.json' or "part" not in file.name:
+        continue
+    with open(file, 'r') as f:
+        partial_results = json.load(f)
+    dict_update_rec(total_results, partial_results)
+    tmp_file_name = Path(TEST_PATH, test_name, test_name + '.tmp')
+    save_into_json(total_results, tmp_file_name, 'w+')
+    file.unlink()
 
-        # delete the old result file and rename the temp one
-        try:
-            os.remove(results_file_name)
-        except OSError:
-            pass
-        os.rename(tmp_file_name, results_file_name)
+    # delete the old result file and rename the temp one
+    results_file_name.unlink()
+    tmp_file_name.rename(results_file_name)
