97e38042ad0d9689fcb7a93745130c9a2eae985e
vojta <451866@mail.muni.cz>
2020-12-07 14:58:24 +0100

    Rename params to default.params and change its format to JSON, close #24

--- curve_analyzer/tests/gen_params.py
+++ curve_analyzer/tests/gen_params.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env sage
 
 from pathlib import Path
+import json
 
 from curve_analyzer.definitions import TEST_PATH
 
 def main():
-    with open(Path(TEST_PATH, 'params'), "r") as f:
-        params = f.read()
-    params = params.split("&test ")[1:]
-
+    with open(Path(TEST_PATH, 'default.params'), "r") as f:
+        params = json.load(f)
     for test in params:
-        name, to_write = test.split(":", 1)
+        name, to_write = test, params[test]
         Path(TEST_PATH, name).mkdir(parents=True, exist_ok=True)
-        to_write = to_write.strip()
         full_name = Path(TEST_PATH, name, name + ".params")
         if full_name.is_file():
             with open(full_name, 'r') as f:
                 current = f.read()
             if not current == to_write:
                 with open(full_name, 'w') as f:
-                    f.write(to_write)
+                    json.dump(to_write,f)
                 print("Params file updated for", name)
         else:
             with open(full_name, 'w') as f:
-                f.write(to_write)
+                json.dump(to_write,f)
             print("Params file created for", name)
 
 if __name__ == '__main__':
