28926cecabf9635ff5a043888e487233df8166f1
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2020-12-14 18:40:25 +0100

    Use the curve equation and division polynomials to eliminate variables.

--- curve_analyzer/utils/zvp/gen_zvp.py
+++ curve_analyzer/utils/zvp/gen_zvp.py
@@ -1,6 +1,6 @@
 import operator
 
-from sage.all import ZZ, Integer, PolynomialRing, QuotientRing
+from sage.all import ZZ, Integer, PolynomialRing, QuotientRing, EllipticCurve
 
 ops = {
     "+": operator.add,
@@ -37,7 +37,8 @@ def add_atomic(zvp_set, value, quotient_ring):
         rad = value.radical()
     if not isinstance(rad, Integer):
         for f, m in rad.factor():
-            zvp_set.add(quotient_ring(f))
+            if not quotient_ring(-f) in zvp_set:
+                zvp_set.add(quotient_ring(f))
 
 
 def add_to_zvp(zvp_set, op, value1, value2, quotient_ring):
@@ -63,7 +64,7 @@ def fill_register(register, formula_file, quotient_ring=ZZ):
 
 
 def main():
-    R = PolynomialRing(ZZ, ('a', 'b', 'x1', 'y1', 'x2', 'y2'), order='invlex')
+    R = PolynomialRing(ZZ, ('a', 'b', 'x1', 'y1', 'x2', 'y2'), order='invlex(4), lex(2)')
     a, b, x1, y1, x2, y2, = R.gens()
     Q = QuotientRing(R, R.ideal(y1 ** 2 - x1 ** 3 - a * x1 - b, y2 ** 2 - x2 ** 3 - a * x2 - b),
                      ('a', 'b', 'x_1', 'y_1', 'x_2', 'y_2'))
@@ -71,10 +72,31 @@ def main():
     zvp_set = fill_register(register, formula_file='addition_formula_1.txt', quotient_ring=Q)
     print("Intermediate values:")
     print(register)
-    print("ZVP conditions:")
+    print("ZVP conditions for general points:")
     zvp_lifted = [x.lift() for x in zvp_set]
-    print(sorted(zvp_lifted, key=lambda x: len(str(x))))
+    zvp_sorted = sorted(zvp_lifted, key=lambda x: len(str(x)))
+    print(zvp_sorted)
 
+    E = EllipticCurve(R, [a, b])
+    mult = E.multiplication_by_m(3)
+    eval_mult = lambda f, x, y: [fi(x, y) for fi in f]
+    x2_subst, y2_subst = eval_mult(mult, x1, y1)
+    # print(x2_subst, y2_subst)
+    print("ZVP conditions for (x2,y2) = c*(x1,y1):")
+    zvp_substited = [x(x2=x2_subst, y2=y2_subst) for x in zvp_sorted]
+    zvp_numerators_lifted = [Q(x.numerator()).lift().factor() for x in zvp_substited]
+    # for f in zvp_substited:
+    #     print(f)
+    #
+    # print("nums")
+    # for f in zvp_numerators_lifted:
+    #     print(f)
+
+    for x in zvp_sorted:
+        print(x)
+        print(x(x2=x2_subst, y2=y2_subst))
+        print(Q(x(x2=x2_subst, y2=y2_subst).numerator()).lift().factor())
+        print("")
 
 if __name__ == '__main__':
     main()
