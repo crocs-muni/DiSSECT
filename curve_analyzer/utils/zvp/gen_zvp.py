52ff4e4a782d9afd5e632bba73b9684080117a22
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2020-12-18 13:21:54 +0100

    Eliminate y1 and constants from conditions, refactor functions, improve verbose output.

--- curve_analyzer/utils/zvp/gen_zvp.py
+++ curve_analyzer/utils/zvp/gen_zvp.py
@@ -23,7 +23,7 @@ class ZVPFinder:
     aa, bb, x_1, x_2, y_1, y_2 = Q.gens()
     E = EllipticCurve(R, [a, b])
 
-    def __init__(self, formula_file, multiple):
+    def __init__(self, formula_file, multiple, verbose=False):
         self.formula_file = formula_file
         self.multiple = multiple
         self.register = {"X1": self.x1, "Y1": self.y1, "X2": self.x2, "Y2": self.y2, "Z1": 1, "Z2": 1, "a": self.a,
@@ -42,6 +42,9 @@ class ZVPFinder:
                 self.add_atomic(f, self.zvp_reduced)
         self.zvp_reduced_lifted = [x.lift() for x in self.zvp_reduced]
         self.zvp_reduced_sorted = sorted(self.zvp_reduced_lifted, key=lambda x: len(str(x)))
+        self.zvp_univariate = self.make_conditions_univariate()
+        if verbose:
+            self.print_zvp_conditions()
 
     def interpret_symbol(self, symbol):
         if symbol.isdigit():
@@ -98,7 +101,7 @@ class ZVPFinder:
                 self.register[lhs] = self.eval_reg_binary(rhs)
         return self.zvp_set
 
-    def print_zero_conditions(self):
+    def print_zvp_conditions(self):
         print("ZVP conditions for general affine points on E: y^2 = x^3 + ax + b:")
         for n in self.zvp_sorted:
             print(n)
@@ -107,44 +110,76 @@ class ZVPFinder:
         print("x2 =", self.x2_subst)
         print("y2 =", self.y2_subst, "\n")
 
-        print("Thus the ZVP conditions become:")
-        for n in self.zvp_reduced_sorted:
+        print("Thus the ZVP conditions become (after eliminating linear occurences of y1):")
+        for n in self.zvp_univariate:
             print(n)
 
     def __str__(self):
         return '{self.zvp_reduced_sorted}'.format(self=self)
 
-    def find_points(self, a, b, q, verbose=False):
-        """Solves ZVP conditions and finds corresponding curve points. Only works for univariate conditions (i.e.,
-        y1 should not be present if x1 is). """
-        E = EllipticCurve(GF(q), [a, b])
-        zvp_roots = []
+    def make_conditions_univariate(self):
+        """Eliminate y1 from the conditions and return univariate conditions instead (though they are still not
+        recognized as univariate until a,b values are substitued in). """
+        zvp_univariate = []
         for p in self.zvp_reduced_sorted:
-            try:
-                roots_with_multiplicity = p(a=a, b=b).univariate_polynomial().roots(GF(q))
-            except (TypeError, AttributeError):
-                continue
-            if verbose:
-                print(roots_with_multiplicity)
-            for root, m in roots_with_multiplicity:
-                if E.is_x_coord(root) and E.lift_x(root) not in zvp_roots:
-                    zvp_roots.append(E.lift_x(root))
-        return zvp_roots
+            if self.y1 in p.variables():
+                # since the polynomial is at most linear in y1, we can solve for y1 and use the curve equation
+                p_wrt_y = p.polynomial(self.y1)
+                roots_wrt_y = p_wrt_y.roots(p_wrt_y.base_ring().fraction_field())
+                assert len(roots_wrt_y) == 1
+                r, _ = roots_wrt_y[0]
+                # use the curve equation as a rational function and substitute for y1
+                curve_function = (self.y1 ** 2 - self.x1 ** 3 - self.a * self.x1 - self.b) / 1
+                p_univariate = curve_function(y1=r).numerator()
+            else:
+                p_univariate = p
+            zvp_univariate.append(p_univariate)
+        return zvp_univariate
+
+    def evaluate_univariate_conditions(self, a, b, q):
+        """Evaluates the univariate conditions at a,b and returns a list of those that remain nonconstant or are zero
+        (as polynomials over GF(q)). """
+        zvp_evaluated_nonconst = []
+        for p in self.zvp_univariate:
+            p_eval = p(a=a, b=b).univariate_polynomial().change_ring(PolynomialRing(GF(q), 'x'))
+            # print(p_eval, p_eval.parent())
+            if not p_eval.is_constant() or p_eval == 0:
+                zvp_evaluated_nonconst.append(p_eval)
+        return zvp_evaluated_nonconst
+
+    def find_points(self, a, b, q, verbose=False):
+        """Solves ZVP conditions and finds corresponding curve points. Only works properly for nonzero univariate
+        conditions (i.e., y1 should not be present if x1 is). """
+        if verbose:
+            print("\nFinding ZVPs over finite field with", q, "elements for a =", a, "and b =", b, ":")
+        E = EllipticCurve(GF(q), [a, b])
+        zero_value_points = []
+        zvp_evaluated_nonconst = self.evaluate_univariate_conditions(a, b, q)
+        for p in zvp_evaluated_nonconst:
+            if p == 0:
+                zero_value_points.append("Any point, since a,b already satisfy a ZVP condition")
+            else:
+                roots_with_multiplicity = p.roots(GF(q))
+                if verbose:
+                    print("Polynomial:", p, "\nRoots:", roots_with_multiplicity)
+                for root, m in roots_with_multiplicity:
+                    if E.is_x_coord(root) and E.lift_x(root) not in zero_value_points:
+                        zero_value_points.append(E.lift_x(root))
+                        if verbose:
+                            print("Corresponding curve point:", E.lift_x(root))
+        return zero_value_points
 
 
 def main():
     from curve_analyzer.definitions import EFD_PATH
     from pathlib import Path
     formula_file = Path(EFD_PATH, 'shortw', 'projective', 'addition', 'add-2016-rcb.op3')
-    ZVP = ZVPFinder(formula_file, multiple=3)
-    ZVP.print_zero_conditions()
+    ZVP = ZVPFinder(formula_file, multiple=2, verbose=True)
     q = next_prime(2 ** 256)
     a = 1
     b = 2
-    print("\nFinding ZVP roots over finite field with", q, "elements for a =", a, "and b =", b, ":")
-    roots = ZVP.find_points(a, b, q, verbose=True)
-    print("\nCorresponding valid curve points:")
-    print(roots)
+    points = ZVP.find_points(a, b, q, verbose=True)
+    print(points)
 
 
 if __name__ == '__main__':
