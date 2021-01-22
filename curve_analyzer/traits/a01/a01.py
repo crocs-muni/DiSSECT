591df033061ea2c731fc555705bb5f7ef85fed7c
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2021-01-22 14:26:25 +0100

    Refactor a01 and take care of edge cases.

--- curve_analyzer/traits/a01/a01.py
+++ curve_analyzer/traits/a01/a01.py
@@ -1,27 +1,31 @@
-from sage.all import GF, Integer
+from sage.all import GF, ZZ
 
 from curve_analyzer.traits.trait_interface import pretty_print_results, compute_results, timeout
 
-# global time for one factorization
+# global time for one group computation
 TIME = 10
 
 
+def compute_second_generator_order(E_ext):
+    '''compute the second group generator order only (if it exists), in order to have synergy with timeout'''
+    gens = E_ext.abelian_group().gens()
+    if len(gens) == 1:
+        return 1
+    else:
+        return gens[1].order
+
 def a01_curve_function(curve, deg):
     """returns the orders of the two generators of the curve over the deg-th relative extension"""
+    curve_results = {}
+    t = TIME
     E = curve.EC
     q = curve.q
-    curve_results = {}
-
     E_ext = E.base_extend(GF(q ** deg))
-    t = TIME
-    curve_results['ord1'] = timeout(E_ext.abelian_group().gens()[0].order, [], timeout_duration=t)
-    if not isinstance(curve_results['ord1'],Integer):
-        curve_results['ord2'] = 1
-        return curve_results
-    try:
-        curve_results['ord2'] = timeout(E_ext.abelian_group().gens()[1].order, [], timeout_duration=t)
-    except IndexError:
-        curve_results['ord2'] = 1
+    curve_results['ord2'] = timeout(compute_second_generator_order, [E_ext], timeout_duration=t)
+    if isinstance(curve_results['ord2'], int):
+        curve_results['ord1'] = ZZ(E_ext.cardinality() / curve_results['ord2'])
+    else:
+        curve_results['ord1'] = None
     return curve_results
 
 
