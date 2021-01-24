c1ac1179e63e46052c117879c0bfec8dc83caba2
Antonín Dufka <xdufka1@fi.muni.cz>
2021-01-24 15:17:16 +0100

    Add dataframe loading

---
+++ curve_analyzer/utils/data_processing.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from typing import Dict, Any
+
+import curve_analyzer.utils.database_handler as database
+
+db = None
+
+
+def load_trait(trait: str, params: Dict[str, Any] = None, curve: str = None) -> pd.DataFrame:
+    global db
+    if not db:
+        db = database.connect()
+
+    return pd.DataFrame(database.get_trait_results(db, trait))
+
+
+def load_curves(filters: Any = {}) -> pd.DataFrame:
+    global db
+    if not db:
+        db = database.connect()
+
+    def project(record: Dict[str, Any]):
+        projection = {}
+        projection["curve"] = record["name"]
+        projection["cofactor"] = int(record["cofactor"], base=16) if isinstance(record["cofactor"], str) else int(record["cofactor"])
+        projection["bitlength"] = record["field"]["bits"]
+        projection["simulated"] = record["simulated"]
+        return projection
+
+    df = pd.DataFrame(map(project, database.get_curves(db, filters, raw=True)))
+    return df
