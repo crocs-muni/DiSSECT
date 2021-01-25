84906e73006c80f787bddcb98ff0560c734cfc00
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2021-01-25 13:04:54 +0100

    Change cofactor to max_cofactor.

--- curve_analyzer/utils/data_processing.py
+++ curve_analyzer/utils/data_processing.py
@@ -32,7 +32,7 @@ def load_curves(filters: Any = {}) -> pd.DataFrame:
     return df
 
 
-def filter_df(df, bitlength, cofactor):
+def filter_df(df, bitlength, max_cofactor):
     df = df[df["bitlength"] == bitlength]
-    df = df[df["cofactor"] == cofactor]
+    df = df[df["cofactor"] <= max_cofactor]
     return df
