9468d7af38c087704a2d7979ddf07b6be6261880
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2021-01-25 14:16:37 +0100

    Allow filtering by sources.

--- curve_analyzer/utils/data_processing.py
+++ curve_analyzer/utils/data_processing.py
@@ -3,6 +3,7 @@ from typing import Dict, Any
 import pandas as pd
 
 import curve_analyzer.utils.database_handler as database
+from curve_analyzer.definitions import STD_CURVE_DICT
 
 db = None
 
@@ -32,7 +33,9 @@ def load_curves(filters: Any = {}) -> pd.DataFrame:
     return df
 
 
-def filter_df(df, bitlength, max_cofactor=1):
+def filter_df(df, bitlength, sources, max_cofactor=1):
+    for source in sources:
+        df = df[df.curve.isin(STD_CURVE_DICT[source])]
     df = df[df["bitlength"] == bitlength]
     df = df[df["cofactor"] <= max_cofactor]
     return df
