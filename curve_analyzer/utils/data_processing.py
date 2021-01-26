3463e854f6a42281b77df2960311f037439742e5
Vladimír Sedláček <vlada.sedlacek@mail.muni.cz>
2021-01-26 17:44:44 +0100

    Convert values to ints/Integers when possible.

--- curve_analyzer/utils/data_processing.py
+++ curve_analyzer/utils/data_processing.py
@@ -16,7 +16,7 @@ def load_trait(trait: str, params: Dict[str, Any] = None, curve: str = None) -> 
     if not db:
         db = database.connect()
 
-    return pd.DataFrame(database.get_trait_results(db, trait))
+    return pd.DataFrame(database.get_trait_results(db, trait)).convert_dtypes()
 
 
 def load_curves(filters: Any = {}) -> pd.DataFrame:
@@ -27,9 +27,8 @@ def load_curves(filters: Any = {}) -> pd.DataFrame:
     def project(record: Dict[str, Any]):
         projection = {}
         projection["curve"] = record["name"]
-        projection["cofactor"] = int(record["cofactor"], base=16) if isinstance(record["cofactor"], str) else int(
-            record["cofactor"])
-        projection["bitlength"] = record["field"]["bits"]
+        projection["cofactor"] = ZZ(record["cofactor"])
+        projection["bitlength"] = ZZ(record["field"]["bits"])
         projection["simulated"] = record["simulated"]
         return projection
 
