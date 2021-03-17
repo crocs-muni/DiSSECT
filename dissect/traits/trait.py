63da124bfc298f4f2b195ee6f11db9e59e5736bd
vojta <451866@mail.muni.cz>
2021-03-17 15:59:35 +0100

    console menu added to trait, close #43

--- dissect/traits/trait.py
+++ dissect/traits/trait.py
@@ -3,6 +3,8 @@ import dissect.traits.gen_unittest as unittest
 import dissect.traits.gen_trait_structures as structures
 import os
 import dissect.utils.json_handler as json_handler
+import consolemenu
+from consolemenu.items import *
 
 
 def create_directory(name):
@@ -36,56 +38,86 @@ def check_files(name):
         break
 
 
-def update_default_params(name):
+def update_default_params(name, delete=False):
     dp_path = os.path.join(TRAIT_PATH, "default.params")
     dp = json_handler.load_from_json(dp_path)
-    p = json_handler.load_from_json(os.path.join(TRAIT_PATH, name, name + ".params"))
-    dp[name] = p
+    if delete:
+        del dp[name]
+    else:
+        dp[name] = json_handler.load_from_json(os.path.join(TRAIT_PATH, name, name + ".params"))
     json_handler.save_into_json(dp, dp_path, mode="w")
 
 
-def update_descriptions(name, desc):
+def update_descriptions(name, desc=None,delete=False):
     descs_path = os.path.join(TRAIT_PATH, "trait_descriptions")
     descs = json_handler.load_from_json(descs_path)
-    descs[name] = desc
+    if delete:
+        del descs[name]
+        del TRAIT_DESCRIPTIONS[name]
+    else:
+        descs[name] = desc
+        TRAIT_DESCRIPTIONS[name]=desc
     json_handler.save_into_json(descs, descs_path, mode="w")
 
 
-def main():
-    welcome = "Welcome to Curve analyzer! You can create or modify traits. Please input a name of trait: "
-    name = input(welcome)
-    if name in TRAIT_DESCRIPTIONS:
-        print("There is a trait", name, "(" + TRAIT_DESCRIPTIONS[name] + ")")
-        choice = input(
-            "Do you want to:\n"
-            + "[1] Generate a trait structure\n"
-            + "[2] Manually create new trait structure\n"
-            + "[3] Generate a unittest\n"
-        )
-        if choice == "1":
-            structures.compute_results(trait_name=name)
-        if choice == "2":
-            unittest.main(trait_name=name, u=False, s=True)
-        if choice == "3":
-            unittest.main(trait_name=name, u=True, s=False)
-        return
-    print("Oh yeah, new trait!")
+def create_trait():
+    name = input("Oh yeah, new trait! Type the name: ")
     create_directory(name)
     check_files(name)
     update_default_params(name)
-    print(
-        "We need to create structure file. Do you with to create one:\n",
-        "[1] manually (you trust your implementation) \n",
-        "[2] automatically (quicker)",
-    )
-    if input() == "1":
-        unittest.main(trait_name=name, u=False, s=True)
+
+    if not 'm'==input("We need to create structure file. Do you wish to create one automatically (you trust your implementation) or manually (slower)? [A/m]").lower():
+        structures.compute_results(name)
     else:
-        structures.compute_results(trait_name=name)
-    if input("Do you wish to auto-generate unittest?[Y/n]").lower() != "n":
+        unittest.main(name, False,True)
+    if input("Do you  wish to auto-generate unittest?[Y/n]").lower() != "n":
         unittest.main(trait_name=name, u=True, s=False)
     desc = input("Please write a short description for your trait: ")
     update_descriptions(name, desc)
+    input("Awesome! Trait "+name+" is ready to go!")
+
+def modify_description(name):
+    desc = input("Write a short description for your trait: ")
+    update_descriptions(name, desc)
+
+def modify_trait(menu):
+    name = input("Type the name of the trait: ")
+    modify_menu = consolemenu.ConsoleMenu("Trait "+name,TRAIT_DESCRIPTIONS[name])
+    modify_menu.parent = menu
+
+    gen_struct = FunctionItem("Generate a trait structure", structures.compute_results,[name])
+    modify_menu.append_item(gen_struct)
+
+    gen_struct = FunctionItem("Manually create new trait structure", unittest.main, [name,False,True])
+    modify_menu.append_item(gen_struct)
+
+    gen_struct = FunctionItem("Generate a unittest", unittest.main, [name,True,False])
+    modify_menu.append_item(gen_struct)
+
+    gen_struct = FunctionItem("Modify the description", modify_description, [name])
+    modify_menu.append_item(gen_struct)
+
+    modify_menu.show()
+
+def delete_trait():
+    name = input("Type the name of the trait: ")
+    if not input("Are you sure you want to delete trait "+name+"?[Y/n]: ").lower()=='y':
+        return
+    update_default_params(name, delete=True)
+    update_descriptions(name, delete=True)
+    input('You can remove the trait folder and the unittest.')
+
+def main():
+    menu = consolemenu.ConsoleMenu("DiSSECT", "Welcome to DiSSECT! You can create or modify traits.")
+    function_item = FunctionItem("Create a trait", create_trait)
+    menu.append_item(function_item)
+    function_item2 = FunctionItem("Modify existing trait", modify_trait,[menu])
+    menu.append_item(function_item2)
+    function_item3 = FunctionItem("Delete existing trait", delete_trait)
+    menu.append_item(function_item3)
+    menu.show()
+
+
 
 
 if __name__ == "__main__":
