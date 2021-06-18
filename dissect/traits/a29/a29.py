487081a3c77b705945bc666fb451972f39d16e54
Vojtěch Suchánek <suchanek.mail@gmail.com>
2021-06-18 14:34:57 +0200

    prepare a29 for nonweierstrass curves

--- dissect/traits/a29/a29.py
+++ dissect/traits/a29/a29.py
@@ -8,8 +8,10 @@ def a29_curve_function(curve: CustomCurve):
     Computation of the torsion order of E' where E' is the lift of the givne
     curve to Q. By lift, we mean lift of coefficients to ZZ
     """
-
-    a,b = ZZ(curve.params['a']['raw']),ZZ(curve.params['b']['raw'])
+    try:
+        a,b = ZZ(curve.EC.a4()), ZZ(curve.EC.a6())
+    except TypeError:
+        a,b = ZZ(curve.params['a']['raw']), ZZ(curve.params['b']['raw'])
     E = EllipticCurve(QQ,[a,b])
     return {'Q_torsion':E.torsion_order()}
 
