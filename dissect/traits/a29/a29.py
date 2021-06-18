e37a2016e2a8a4eae2145186f8ffaa6815bee298
Vojtěch Suchánek <suchanek.mail@gmail.com>
2021-06-18 14:07:45 +0200

    a29 prepared for binary curves

--- dissect/traits/a29/a29.py
+++ dissect/traits/a29/a29.py
@@ -8,7 +8,8 @@ def a29_curve_function(curve: CustomCurve):
     Computation of the torsion order of E' where E' is the lift of the givne
     curve to Q. By lift, we mean lift of coefficients to ZZ
     """
-    a,b = ZZ(curve.EC.a4()),ZZ(curve.EC.a6())
+
+    a,b = ZZ(curve.params['a']['raw']),ZZ(curve.params['b']['raw'])
     E = EllipticCurve(QQ,[a,b])
     return {'Q_torsion':E.torsion_order()}
 
