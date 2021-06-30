b1384d89a72d74ebfc8a7103034f7f5f0a5d05ef
Antonín Dufka <xdufka1@fi.muni.cz>
2021-06-30 15:51:37 +0200

    Add utility functions

--- dissect/analysis/data_processing.py
+++ dissect/analysis/data_processing.py
@@ -9,6 +9,8 @@ from sklearn.preprocessing import MinMaxScaler
 
 import dissect.utils.database_handler as database
 from dissect.definitions import STD_CURVE_DICT, ALL_CURVE_COUNT
+from dissect.traits.trait_info import TRAIT_INFO, params_iter, nonnumeric_outputs
+
 
 
 class Modifier:
@@ -38,8 +40,7 @@ class Modifier:
         return lambda x: len(x)
 
 
-def load_trait(
-        source: str, trait: str, params: Dict[str, Any] = None, curve: str = None) -> pd.DataFrame:
+def load_trait(source: str, trait: str, params: Dict[str, Any] = None, curve: str = None, skip_timeouts: bool = False) -> pd.DataFrame:
     if source.startswith("mongodb"):
         trait_results = database.get_trait_results(database.connect(source), trait)
     elif source.startswith("http"):
@@ -49,6 +50,9 @@ def load_trait(
     else:
         with open(file, "r") as f:
             trait_results = json.load(f)
+
+    if skip_timeouts:
+        trait_results = filter(lambda x: "NO DATA (timed out)" not in x.values(), trait_results)
 
     return pd.DataFrame(trait_results).convert_dtypes()
 
@@ -149,3 +153,23 @@ def find_outliers(df, features):
     lof.fit(data)
     predictions = lof.fit_predict(data)
     return df[predictions == -1]
+
+def flatten_trait(trait_name, trait_df):
+    result_df = trait_df[["curve"]].drop_duplicates(subset=["curve"])
+
+    for params in params_iter(trait_name):
+        if params:
+            for param in params:
+                param_df = trait_df[trait_df[param] == params[param]]
+
+            param_df = param_df.drop(params.keys(), axis=1)
+        else:
+            param_df = trait_df
+
+        param_df = param_df.drop(nonnumeric_outputs(trait_name), axis=1)
+        param_df.columns = map(lambda x: "curve" if x == "curve" else f"{x}_{params}", param_df.columns)
+        result_df = result_df.merge(param_df, "left", on="curve")
+
+
+    return result_df
+
