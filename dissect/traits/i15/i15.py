e7327d3ab414a414c051bfdddef217f4611886b5
Vojtěch Suchánek <suchanek.mail@gmail.com>
2021-08-30 10:50:11 +0200

    extension exception i15

--- dissect/traits/i15/i15.py
+++ dissect/traits/i15/i15.py
@@ -4,8 +4,10 @@ from dissect.utils.custom_curve import CustomCurve
 
 
 def i15_curve_function(curve: CustomCurve):
-    return {"a": ZZ(curve.a()), "b": ZZ(curve.b())}
-
+    try:
+        return {"a": ZZ(curve.a()), "b": ZZ(curve.b())}
+    except (TypeError, ValueError):
+        return {"a":None, "b":None}
 
 def compute_i15_results(curve_list, desc="", verbose=False):
     compute_results(curve_list, "i15", i15_curve_function, desc=desc, verbose=verbose)
