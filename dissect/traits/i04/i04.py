11f68eb1208f457544c1bc3ce9f450b9f4c891c6
Vojtěch Suchánek <suchanek.mail@gmail.com>
2021-08-30 10:37:06 +0200

    improve hamming weight computation

--- dissect/traits/i04/i04.py
+++ dissect/traits/i04/i04.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 
-from sage.all import ZZ, Combinations
+from sage.all import ZZ, binomial
 
 from dissect.traits.trait_interface import compute_results
 from dissect.utils.custom_curve import CustomCurve
 from dissect.utils.json_handler import FLOAT_PRECISION
 
+def next_hamming(val):
+    c = val & -val
+    r = val + c
+    return ZZ((((r ^ val) >> 2) // c) | r)
 
 def i04_curve_function(curve: CustomCurve, weight):
     """Computes the number of curve points whose x-coord has the given Hamming weight"""
     bit_length = ZZ(curve.cardinality()).nbits()
     E = curve.ec()
     x_coord_count = 0
-    combination_list = Combinations(range(bit_length), weight).list()
-    for combination in combination_list:
-        binary = "0" * bit_length
-        for bit in combination:
-            binary = binary[:bit] + "1" + binary[bit + 1 :]
-        x_coord = ZZ("0b" + binary)
+    x_coord = ZZ(2**weight-1)
+    while True:
+        if x_coord.nbits()>bit_length:
+            break
         if E.is_x_coord(x_coord):
-            x_coord_count += 1
-    expected = len(combination_list) // 2
+            x_coord_count+=1
+        x_coord = next_hamming(x_coord)
+    expected = binomial(bit_length,weight) // 2
     ratio = expected / x_coord_count
     curve_results = {
         "x_coord_count": x_coord_count,
