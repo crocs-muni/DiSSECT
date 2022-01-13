5dc305162b62ac3a1fd210c9a1f1f9656dd8a960
Vojtěch Suchánek <suchanek.mail@gmail.com>
2022-01-13 14:40:34 +0100

    a01 simplified

--- dissect/traits/a01/a01.py
+++ dissect/traits/a01/a01.py
@@ -1,31 +1,11 @@
-from sage.all import gcd, lcm
-
-from dissect.traits.trait_interface import compute_results, timeout
+from dissect.traits.trait_interface import compute_results
 from dissect.utils.custom_curve import CustomCurve
 
-# global time for one group computation
-
-TIME = 150
 
 
-def smith_normal_form(ext_ec):
-    """compute the smith normal form, (n1,n2) where n1 divides n2"""
-    gens = ext_ec.abelian_group().gens()
-    if len(gens) == 1:
-        return 1, gens[0].order()
-    ord1, ord2 = gens[0].order(), gens[1].order()
-    return gcd(ord1, ord2), lcm(ord1, ord2)
-
-
-def a01_curve_function(curve: CustomCurve, deg):
-    """returns the orders of the two generators of the curve over the deg-th relative extension"""
-    curve_results = {}
-    ext_ec = curve.extended_ec(deg)
-    result = timeout(smith_normal_form, [ext_ec], timeout_duration=TIME)
-    if isinstance(result, str):
-        result = result, result
-    curve_results["ord1"], curve_results["ord2"] = result
-    return curve_results
+def a01_curve_function(curve: CustomCurve):
+    """Returns the order of the prime order subgroup and its cofactor"""
+    return {"order":curve.order(), "cofactor":curve.cofactor()}
 
 
 def compute_a01_results(curve_list, desc="", verbose=False):
